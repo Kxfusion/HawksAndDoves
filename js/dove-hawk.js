662c6f687bbc567e93b9dce73d873ef6f33523e8
tmirkovic <tibormirkovic@gmail.com>
2017-03-06 20:59:52 -0500

    GUI now shows when a bird gets eaten by a hawk

--- js/dove-hawk.js
+++ js/dove-hawk.js
@@ -8,21 +8,34 @@ var gridHeight = 10;
 var gridWidth = 10;
 
 //generate the initial grid
-generateGridArray(gridWidth, gridHeight);
+generateGridArray();
 
 //render the grid
 renderGridArray()
 
-//every 5 seconds, update the grid
+//every 5 seconds, start a new turn
 setInterval(function() {
 	nextTick();
-}, 5000);
+}, 10000);
 
 //updates the grid
 function nextTick() {
 
-	updateGrid(gridWidth, gridHeight);
+	//mark all relevant birds for death
+	killBirds();
+
+	//update the GUI so that we can see which birds are alive
 	renderGridArray()
+
+	//five seconds later we replace the birds with new one's
+	setTimeout(function() {
+
+		//replace all birds marked for death with new birds
+		replaceBirds();
+
+		//update the GUI
+		renderGridArray();
+	}, 5000)
 }
 
 //returns a string, either dove or hawk
@@ -36,25 +49,23 @@ function chooseBirdType() {
 	}
 }
 
-function updateGrid(Width, Height) {
-	var checkGrid = [];
-	for (var a = 0; a < Width; a++) {
-		checkGrid[a] = [];
-		for (var b = 0; b < Height; b++) {
-			checkGrid[a][b] = false;
+//loop through the grid, mark all the the appropriate birds as dead
+function killBirds() {
+	for (var i = 0; i < gridHeight; i++) {
+		for (var j = 0; j < gridWidth; j++) {
+			gridArray[i][j].isDead = determineLife(i, j, true);
 		}
 	}
-	for (var i = 0; i < Height; i++) {
-		for (var j = 0; j < Width; j++) {
-			checkGrid[j][i] = determineLife(i, j, true);
-		}
-	}
+}
 
-	for (var i = 0; i < Height; i++) {
-		for (var j = 0; j < Width; j++) {
-			if (checkGrid[i][j] == true) {
+//replace all the dead birds with new ones
+function replaceBirds() {
+	for (var i = 0; i < gridHeight; i++) {
+		for (var j = 0; j < gridWidth; j++) {
+			if (gridArray[i][j].isDead == true) {
 
-				gridArray[i][j].birdType = chooseBirdType();
+				gridArray[i][j].birdType = chooseBirdType(); //give it a new randomly selected bird type
+				gridArray[i][j].isDead = false; //bring the bird back to life!
 
 			}
 		}
@@ -65,10 +76,11 @@ function updateGrid(Width, Height) {
 function determineLife(x, y, first) {
 	if (first == true) {
 		first = false;
-		var up = determineLife(y - 1, x, first);
-		var down = determineLife(y + 1, x, first);
-		var right = determineLife(y, x - 1, first);
-		var left = determineLife(y, x + 1, first);
+		var up = determineLife(x + 1, y, first);
+		var down = determineLife(x - 1, y, first);
+		var right = determineLife(x, y + 1, first);
+		var left = determineLife(x, y - 1, first);
+
 		if (up == true || down == true || right == true || left == true) {
 			//dead
 			return true;
@@ -76,30 +88,34 @@ function determineLife(x, y, first) {
 			//alive
 			return false;
 		}
-	} else if (x < gridWidth && y < gridHeight && x != -1 && y != -1) {
+	} else if (x < gridHeight && y < gridWidth && x != -1 && y != -1) {
 		if (gridArray[x][y].birdType == 'hawk') {
+			//encountering a hawk, we are dead
 			return true;
 		} else {
+			//encountering a dove, we are alive (for now)
 			return false;
 		}
 	} else {
+		//nothing to this side, so we are alive
 		return false;
 	}
 }
 
-function generateGridArray(width, height) {
+//this is called to initially populate the array with birds
+function generateGridArray() {
 
 	//clear the array
 	gridArray = [];
 
-
-	for (var i = 0; i < height; i++) {
+	for (var i = 0; i < gridHeight; i++) {
 		var row = [];
-		for (var j = 0; j < width; j++) {
+		for (var j = 0; j < gridWidth; j++) {
 
-			//flip a coin, decide if bird is hawk or a dove
+			//add an object representing the bird to the array
 			row.push({
-				birdType: chooseBirdType(),
+				birdType: chooseBirdType(), //flip a coin, decide if bird is hawk or a dove
+				isDead: false //all birds start out alive
 			});
 
 
@@ -108,17 +124,24 @@ function generateGridArray(width, height) {
 	}
 }
 
-
+//this will update the GUI table
 function renderGridArray() {
-	//build string?
+	//we build an html string
 	var newGridHtml = '';
 	for (var i = 0; i < gridArray.length; i++) {
 		newGridHtml += '<div class="row">';
 		for (var j = 0; j < gridArray[i].length; j++) {
 
-			newGridHtml += '<img src="images/';
-			newGridHtml += gridArray[i][j].birdType;
-			newGridHtml += '.png">';
+			//set the text for when we hover on a table cell
+			var hoverText = gridArray[i][j].birdType + " (" + i + "," + j + ")";
+
+			newGridHtml += '<a title="' + hoverText + '"><img src="images/' + gridArray[i][j].birdType;
+
+			if (gridArray[i][j].isDead == true) {
+				newGridHtml += "_dead";//use the version of the image with an X through it
+			}
+
+			newGridHtml += '.png"></a>';
 
 		}
 		newGridHtml += '</div>';
